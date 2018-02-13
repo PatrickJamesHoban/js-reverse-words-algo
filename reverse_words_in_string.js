d078816ad2d55e9a792c307082eb5c5230025959
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-13 14:31:23 -0600

    initial commit updated readme and outline of pseudocode

---
+++ reverse_words_in_string.js
@@ -0,0 +1,16 @@
+// PSEUDO CODE
+// Split the string into an array based on the space btwn each word.
+// Reverse the array.
+// Join the words in the array back into a string.
+// Remove any blank space on the end.
+
+
+// CODE
+function reverseWords(str){
+  return str; // reverse those words
+}
+
+
+
+// DRIVER CODE
+reverseWords("The greatest developer in all the land would need practice to become great")  // should return "great become to practice need would land the all in developer greatest The"
